dee89ae7da3127a4f40d3857fffae58acbfd5692
cleber-s <43525485+cleber-s@users.noreply.github.com>
2018-09-24 02:08:23 -0300

    Add files via upload

---
+++ classificados/index.php
@@ -0,0 +1,18 @@
+<?php require 'pages/header.php'; ?>
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h2>Nós temos hoje 99 anúncios</h2>
+			<p>E mais de 99 usuários cadastrados</p>
+		</div><!---jumbotron--->
+
+		<div class="row">
+			<div class="col-sm-3">
+				<h4>Pesquisa Avançada</h4>
+			</div>
+			<div class="col-sm-9">
+				<h4>Últimos Anúncios</h4>
+			</div>
+		</div>
+	</div><!---container-fluid--->
+
+<?php require 'pages/footer.php'; ?>
